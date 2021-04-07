16293fa23b02749782b24ec81fbe69f21a5248d4
NyiratiTamas <tamas.nyirati@gmail.com>
2021-04-07 18:09:56 +0200

    ures

---
+++ gitModulZaroGyak/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitModulZaroGyak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
