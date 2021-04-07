67f024635c606db596e859fc2de7307bcfbf60ce
NyiratiTamas <2tamas.nyirati@gmail.comgit config --global user.email tamas.nyirati@gmail.comgit config --global user.email 2tamas.nyirati@gmail.com>
2021-04-07 18:13:44 +0200

    kerdesek

--- gitModulZaroGyak/Program.cs
+++ gitModulZaroGyak/Program.cs
@@ -10,6 +10,21 @@ namespace gitModulZaroGyak
     {
         static void Main(string[] args)
         {
+            //fájlbeolvasás tárolás
+
+            //adatok száma
+            
+            //legnagyobb elem helye
+
+            // van budapesti lakos
+
+            //mindenki 18 feletti
+
+            //ki lakik a XIII. keruletben
+
+            //milyen keruletekben laknak
+
+            //melyi keruletben hanyan laknak
         }
     }
 }
