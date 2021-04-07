d7d879092c1faf9bc9079acf25a6723c6165d0f4
NyiratiTamas <2tamas.nyirati@gmail.comgit config --global user.email tamas.nyirati@gmail.comgit config --global user.email 2tamas.nyirati@gmail.com>
2021-04-07 19:21:46 +0200

    budapestiLakos

--- gitModulZaroGyak/Program.cs
+++ gitModulZaroGyak/Program.cs
@@ -11,6 +11,8 @@ namespace gitModulZaroGyak
     {
         static void Main(string[] args)
         {
+            int i = 0;
+
             //fájlbeolvasás tárolás
             string[] sorok = File.ReadAllLines("adatok.txt");
             List<Szemely> szemelyek = new List<Szemely>();
@@ -24,15 +26,18 @@ namespace gitModulZaroGyak
             Console.WriteLine($"A személyek száma: {N}");
             //legnagyobb elem helye
             int korMax = 0;
-            for (int i = 0; i < N; i++)
+            for (i = 0; i < N; i++)
             {
                 if(szemelyek[i].getKor() > korMax) { korMax = i; }
             }
             Console.WriteLine($"A legidősebb személy: { szemelyek[korMax].getNev()}");
 
-            
-            // van budapesti lakos
 
+            // van budapesti lakos
+            i = 0;
+            while (i<N && !(szemelyek[i].getHely() == "Budapest")) { i++; }
+            bool van = i < N;
+            Console.WriteLine(van ? "van" : "nincs");
             //mindenki 18 feletti
 
             //ki lakik a XIII. keruletben
